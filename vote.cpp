36453d3f10430facdf78de11aef0a4e87e0dc580
ReddyHarshaYadhav <113408624+ReddyHarshaYadhav@users.noreply.github.com>
2023-03-07 09:41:57 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"enter your age=";
+    cin>>age;
+    if(age>=18)
+    cout<<"elgeble for vote";
+    else
+    cout<<"not elgeble for vote";
+}
